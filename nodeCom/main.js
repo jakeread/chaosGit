f1a7454b784930ae498dd7d21ab1d9b27c00879e
jakeread <jakerobertread@gmail.com>
2017-02-24 17:09:30 -0500

    main

--- nodeCom/main.js
+++ nodeCom/main.js
@@ -1,9 +1,15 @@
-// var commandPrompt = require('./commandPrompt');
-
+// serial port: talks to the duino
 var serialport = require('serialport'),
 	SerialPort = serialport,
 	portname = '/dev/ttyACM0'; //process.argv[2]; // to read serial port name from command line
 
+//websocket, ? talks to the browser-side
+var WebSocketServer = require('ws').Server;
+var SERVER_PORT = 8081;
+var wss = new WebSocketServer({port: SERVER_PORT});
+var connections = new Array; // handles the multiple connections
+
+//for terminal inputs
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -11,33 +17,30 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
+//--------------------- readline
 rl.on('line', (input) => {
 	console.log('sent to duino: ' + input);
 	myPort.write(input + '\n');
 });
 
+//--------------------- SerialPort
 var myPort = new SerialPort(portname, { 
 	baudrate: 38400,
 	dataBits: 8,
 	parity: 'none',
 	flowControl: false, 
-	parser: serialport.parsers.readline("\r\n")
+	parser: serialport.parsers.readline("\r\n") // sets readline function to call only when new line
 });
 
-// var command = commandPrompt.getCommand();
-
 myPort.on('open', function() {
-	// function that happens when port opens
 	console.log('port is open');
 });
 
 myPort.on('close', function() {
-	// function for closing
 	console.log('port is closed');
 });
 
 myPort.on('error', function() {
-	// when error, fires
 	console.log('error on serialport');
 });
 
@@ -47,4 +50,38 @@ myPort.on('data', function(data) {
 	// now make decisions and do stuff based on data
 });
 
+//--------------------- WebSocketServer
 
+wss.on('connection', handleConnection);
+
+function handleConnection(client) {
+	console.long("New Connection on wss");
+	connections.push(client); // add client to connections array
+	
+	client.on('message', parseClientMessage); // when we get a message, parse & do stuff
+	
+	client.on('close', function() {
+		console.log("Connection closed on wss");
+		var position = connections.indexOf(client); // index of connection in array of connections
+		connections.splice(position, 1); // remove from array
+	});
+}
+	
+function parseClientMessage(data) {
+	console.log("Received from wss: " + data);
+	//myPort.write(data); // send to arduino over serial
+	// only if serial is open? can check? will throw error automatically?
+}
+	
+function sendData(data){
+	for (connection in connections){ // plurals!
+		connections[connection].send(data);
+	}
+}
+	
+function sendToWeb(data){
+	console.log("sentToWeb: "+data);
+	if (connections.length > 0) {
+		broadcast(data);
+	}
+}
