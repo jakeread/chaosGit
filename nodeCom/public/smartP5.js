ddf4eec0ded1899f08a1d5aae8f6983dd1c83f7d
jakeread <jakerobertread@gmail.com>
2017-02-25 10:59:34 -0500

    moved ood to tbd

--- nodeCom/public/smartP5.js
+++ nodeCom/public/smartP5.js
@@ -1,27 +1,44 @@
 var text;
-var y = 100;
+var y = 0;
 var socket = new WebSocket("ws://localhost:8081");
 
 function setup() {
 	// createCanvas must be the first statement
-	createCanvas(720, 400);  
-	stroke(255);     // Set line drawing color to white
-	frameRate(30);
+	canvas = createCanvas(710, 400, WEBGL);  
+	//stroke(0);     // Set line drawing color to white
+	frameRate(24);
+	canvas.position(200,5);
 	
 	socket.onopen = openSocket;
 	socket.onmessage = showData;
 	
 	text = createDiv("Data in: ");
-	text.position(0, 420);
+	text.position(5, 5);
 }
 
 function draw() {
-	background(0);   // Set the background to black
-	y = y - 1; 
-	if (y < 0) { 
-		y = height; 
-	} 
-	line(0, y, width, y);  
+	background(200);   // Set the background to black
+	
+	var radius = width * 1.5;
+	
+	orbitControl();
+	
+	normalMaterial();
+	translate(0, 0, -600);
+	for(var i = 0; i <= 12; i++){
+		for(var j = 0; j <= 12; j++){
+			push();
+			var a = j/12 * PI;
+			var b = i/12 * PI;
+			translate(sin(2 * a) * radius * sin(b), cos(b) * radius / 2 , cos(2 * a) * radius * sin(b));    
+			if(j%2 === 0){
+				cone(30, 30);
+			}else{
+				box(30, 30, 30);
+			}
+			pop();
+		}
+	}
 }
 
 function openSocket() {
